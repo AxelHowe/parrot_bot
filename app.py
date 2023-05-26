61ce0518be8eecaea8c6b8a395c66429f3e9d08b
AxelHowe <75995668+AxelHowe@users.noreply.github.com>
2023-05-26 19:01:48 +0800

    update app.py

--- app.py
+++ app.py
@@ -52,17 +52,17 @@ def callback():
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    # 轉換成 dict 格式
-    req = request.get_json()
-    print(req)
-    # 取得回覆文字
-    reText = req["queryResult"]["fulfillmentText"]
-    print(reText)
-    # 在回覆的文字後方加上(webhook)識別
-    return {
-        "fulfillmentText": f"{reText} (webhook)",
-        "source": "webhookdata"
-    }
+    # 轉換成 dict 格式
+    req = request.get_json()
+    print(req)
+    # 取得回覆文字
+    reText = req["queryResult"]["fulfillmentText"]
+    print(reText)
+    # 在回覆的文字後方加上(webhook)識別
+    return {
+        "fulfillmentText": f"{reText} (webhook)",
+        "source": "webhookdata"
+    }
 
 # line bot 訊息接收處
 @handler.add(MessageEvent, message=TextMessage)
